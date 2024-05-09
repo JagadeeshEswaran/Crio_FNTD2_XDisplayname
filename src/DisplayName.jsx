b3b0cd69441ea035541c170cd4d8498a012e84cf
Jagadeeshwaran Eswaran <106945920+Jaga172023@users.noreply.github.com>
2024-05-09 12:00:30 +0000

    Bug Fix 04

--- src/DisplayName.jsx
+++ src/DisplayName.jsx
@@ -2,72 +2,73 @@
 import React, { useState } from "react";
 
 const DisplayName = () => {
-	const [fullName, setName] = useState({
-		fname: "",
-		lname: "",
-	});
-	const [showName, setShowName] = useState(false);
+  const [fullName, setName] = useState({
+    fname: "",
+    lname: "",
+  });
+  const [showName, setShowName] = useState(false);
 
-	const handleDisplayName = (e) => {
-		e.preventDefault();
+  const handleDisplayName = (e) => {
+    e.preventDefault();
 
-		if (!fullName.fname || !fullName.lname) {
-			alert("Please fillout both the name fields");
-		} else {
-			setShowName(true);
-		}
-	};
+    if (!fullName.fname || !fullName.lname) {
+      alert("Please fillout both the name fields");
+    } else {
+      setShowName(true);
+    }
+  };
 
-	const validateInput = (e) => {
-		const regEx = /[^a-zA-Z]/;
+  const validateInput = (e) => {
+    const regEx = /[^a-zA-Z]/;
 
-		if (regEx.test(e.target.value) === true) {
-			alert("Invalid Input");
+    if (regEx.test(e.target.value) === true) {
+      alert("Invalid Input");
 
-			e.target.value = e.target.value.replace(/[^a-zA-Z]/, "");
-		} else {
-			const { name, value } = e.target;
+      e.target.value = e.target.value.replace(/[^a-zA-Z]/, "");
+    } else {
+      const { name, value } = e.target;
 
-			setName((prevState) => ({ ...prevState, [name]: value }));
-		}
-	};
+      setName((prevState) => ({ ...prevState, [name]: value }));
+    }
+  };
 
-	return (
-		<>
-			<section>
-				<h1>Full Name Display</h1>
+  return (
+    <>
+      <section>
+        <h1>Full Name Display</h1>
 
-				<form
-					style={{ display: "flex", flexDirection: "column" }}
-					onSubmit={handleDisplayName}>
-					<article>
-						<label>First Name:</label>
-						<input
-							type="text"
-							name="fname"
-							id="fName"
-							onChange={validateInput}
-						/>
-					</article>
-					<article>
-						<label>Last Name:</label>
-						<input
-							type="text"
-							name="lname"
-							id="lName"
-							onChange={validateInput}
-						/>
-					</article>
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={handleDisplayName}
+        >
+          <article>
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="fname"
+              id="fName"
+              onChange={validateInput}
+            />
+          </article>
+          <article>
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lname"
+              id="lName"
+              onChange={validateInput}
+            />
+          </article>
 
-					<article>
-						<button type="submit">Submit</button>
-					</article>
-				</form>
+          <article>
+            <button type="submit">Submit</button>
+          </article>
+        </form>
 
-				{showName && `Full Name: ${fullName.fname} ${fullName.lname}`}
-			</section>
-		</>
-	);
+        <p>{showName && `Full Name: ${fullName.fname} ${fullName.lname}`}</p>
+      </section>
+    </>
+  );
 };
 
 export default DisplayName;
